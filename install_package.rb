56b32d66a41d27ddc6f9ff53ccc8f13b43958ca5
JhonnyBravo <sanfranceshika5@gmail.com>
2017-11-06 03:43:59 +0900

    modified: install_package.rb
    日本語入力関連パッケージのインストールを追加。

--- install_package.rb
+++ install_package.rb
@@ -1,5 +1,12 @@
-# package install
-%w(raspi-config build-essential module-assistant linux-headers-3.16.0-4-all fcitx-mozc).each do |pkg|
+# 仮想マシン向けパッケージのインストール
+%w(raspi-config build-essential module-assistant linux-headers-3.16.0-4-all).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# 日本語入力関連パッケージ
+%w(fcitx-mozc fonts-ipafont fonts-ipaexfont fonts-takao).each do |pkg|
   package pkg do
     action :install
   end
