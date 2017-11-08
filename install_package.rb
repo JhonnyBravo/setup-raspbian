e31843625cfe080f6557f4ea48958363ab389770
Jhonny Bravo <sanfranceshika5@gmail.com>
2017-11-08 09:39:35 +0000

    modified: install_package.rb
    raspi-config, fcitx-mozc のインストール処理を削除。

--- install_package.rb
+++ install_package.rb
@@ -1,12 +1,12 @@
 # 仮想マシン向けパッケージのインストール
-%w(raspi-config build-essential module-assistant linux-headers-3.16.0-4-all).each do |pkg|
+%w(build-essential module-assistant linux-headers-3.16.0-4-all).each do |pkg|
   package pkg do
     action :install
   end
 end
 
 # 日本語入力関連パッケージ
-%w(fcitx-mozc fonts-ipafont fonts-ipaexfont fonts-takao).each do |pkg|
+%w(fonts-ipafont fonts-ipaexfont fonts-takao).each do |pkg|
   package pkg do
     action :install
   end
